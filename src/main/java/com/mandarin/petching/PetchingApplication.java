a203a7f2341d1fbb153f4156ff02a9cf49fc27ed
onlwu <prgrmsg@gmail.com>
2022-04-18 22:33:22 +0900

    페이징 완료

---
+++ src/main/java/com/mandarin/petching/PetchingApplication.java
@@ -0,0 +1,13 @@
+package com.mandarin.petching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetchingApplication.class, args);
+	}
+
+}
