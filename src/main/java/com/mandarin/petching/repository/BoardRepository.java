80424d3340314015a4641562ad4f4309098a7e18
onlwu <prgrmsg@gmail.com>
2022-04-18 22:31:31 +0900

    버전 업을 위한 삭제

--- src/main/java/com/mandarin/petching/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mandarin.petching.repository;
-
-import com.mandarin.petching.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
--- src/main/java/com/mandarin/petching/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mandarin.petching.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository {
-}
