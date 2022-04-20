e9686a73b88c52d05b6100e1d97e49d3879b1097
hhhhhsh <jiajia072518@gmail.com>
2022-04-20 13:53:38 +0900

    join and login

---
+++ src/main/java/com/mandarin/petching/controller/MemberController.java
@@ -0,0 +1,76 @@
+package com.mandarin.petching.controller;
+
+import com.mandarin.petching.dto.MemberFormDto;
+import com.mandarin.petching.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mandarin.petching.domain.Member;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@RequestMapping("/members")
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping(value = "/new")
+    public String memberForm(Model model){
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+
+    @PostMapping(value = "/new")
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
+
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "/member/memberLoginForm";
+    }
+    @GetMapping("/success")
+    public ModelAndView loginSuccess(Authentication auth){
+        ModelAndView mav = new ModelAndView("/member/success");
+        System.out.println("Authentication 안에 해당 로그인 사용자 정보 담겨 있음");
+        System.out.println("유저 로그인 성공");
+        return mav;
+    }
+
+    @GetMapping("/logout")
+    public String logout(){
+        System.out.println("유저 로그아웃 성공");
+        return "/member/logout";
+    }
+}
--- src/main/java/com/mandarin/petching/domain/Member.java
+++ src/main/java/com/mandarin/petching/domain/Member.java
@@ -1,37 +1,45 @@
 package com.mandarin.petching.domain;
 
+import com.mandarin.petching.dto.MemberFormDto;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Getter @Setter
 @DiscriminatorColumn
 public class Member {
 
     @Id
+    @Column(name = "member_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // TODO Bean Validation
     private String userId;
-    private String userPwd;
     private String userName;
 
-    private LocalDate userBth;
-
-    @Enumerated(EnumType.STRING)
-    private GenderType userGender;
-
+    @Column(unique = true)
     private String userEmail;
-    private String userTel;
+
+    private String userPwd;
+
+    private String address;
 
     @Enumerated(EnumType.STRING)
-    private MemberType memberType;
+    private Role role;
 
-    private String imageUrl;
-
-    private String chatUrl;
+    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder){
+        Member member = new Member();
+        member.setUserId(memberFormDto.getUserId());
+        member.setUserName(memberFormDto.getUserName());
+        member.setUserEmail(memberFormDto.getUserEmail());
+        member.setAddress(memberFormDto.getAddress());
+        String password = passwordEncoder.encode(memberFormDto.getUserPwd());
+        member.setUserPwd(password);
+        member.setRole(Role.admin);
+        return member;
+    }
 }
