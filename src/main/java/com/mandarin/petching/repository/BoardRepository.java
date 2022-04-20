6b19ab46a77731bac6656d382f941887277b1bf6
이정운 <>
2022-04-20 11:31:33 +0900

    chore:repository

--- src/main/java/com/mandarin/petching/repository/BoardRepository.java
+++ src/main/java/com/mandarin/petching/repository/BoardRepository.java
@@ -1,9 +1,18 @@
 package com.mandarin.petching.repository;
 
 import com.mandarin.petching.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+
 }
+
