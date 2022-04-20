ce8e067df38b08c5ea51e3328999d5b0a9274b7a
seungyeonpark <siaayeon@gmail.com>
2022-04-20 14:29:47 +0900

    Fix: pet repository

---
+++ src/main/java/com/mandarin/petching/repository/PetRepository.java
@@ -0,0 +1,7 @@
+package com.mandarin.petching.repository;
+
+import com.mandarin.petching.domain.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+}
