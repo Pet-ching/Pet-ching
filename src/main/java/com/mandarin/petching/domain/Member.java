9e0927aad991f3c192ce36b97bc4098bff3ee536
이정운 <>
2022-04-20 16:05:35 +0900

    Feat:Fixed

--- src/main/java/com/mandarin/petching/domain/Member.java
+++ src/main/java/com/mandarin/petching/domain/Member.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 @Entity
 @Getter @Setter
-@DiscriminatorColumn
 public class Member {
 
     @Id
@@ -43,6 +42,9 @@ public class Member {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @OneToMany(mappedBy = "member")
+    private List<Board> board = new ArrayList<Board>();
 
 
     public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder){
