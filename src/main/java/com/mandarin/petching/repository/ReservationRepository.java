6f033719124ec3b4c263bd09543deabcb92ca1fc
seungyeonpark <siaayeon@gmail.com>
2022-04-23 15:57:34 +0900

    Feat: reservation repository

--- src/main/java/com/mandarin/petching/repository/ReservationRepository.java
+++ src/main/java/com/mandarin/petching/repository/ReservationRepository.java
@@ -1,15 +1,14 @@
 package com.mandarin.petching.repository;
 
-
+import com.mandarin.petching.domain.PetSitter;
 import com.mandarin.petching.domain.Reservation;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.ui.Model;
 
-import javax.persistence.EntityManager;
+import java.util.List;
 
-@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    List<Reservation> findByPetOwnerId(Long petOwnerId);
+
+    List<Reservation> findByPetSitterId(Long petSitterId);
 }
