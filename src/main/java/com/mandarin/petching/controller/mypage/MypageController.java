458b88e2fcc975ecfce498bfad3f8c90eac5fc3c
seungyeonpark <siaayeon@gmail.com>
2022-04-23 22:59:07 +0900

    Feat: member info

--- src/main/java/com/mandarin/petching/controller/mypage/MypageController.java
+++ src/main/java/com/mandarin/petching/controller/mypage/MypageController.java
@@ -1,6 +1,10 @@
 package com.mandarin.petching.controller.mypage;
 
+import com.mandarin.petching.domain.Member;
+import com.mandarin.petching.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +14,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Slf4j
 @Controller
 @RequestMapping("/mypage")
+@RequiredArgsConstructor
 public class MypageController {
 
-    @GetMapping
-    public String mypage(Model model, @RequestParam String type) {
+    private final MemberRepository memberRepository;
 
+    @GetMapping
+    public String mypage(Authentication authentication, @RequestParam String type, Model model) {
+
+        String userName = authentication.getName();
+        Member member = memberRepository.findByUserEmail(userName);
+
+        model.addAttribute("member", member);
         model.addAttribute("type", type);
         return "mypage/mypage";
     }
