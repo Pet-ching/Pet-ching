0415d6e9a7bbcef8fac49fbcfd04905458e49a3a
seungyeonpark <siaayeon@gmail.com>
2022-04-24 11:14:16 +0900

    Feat: add enum description

--- src/main/java/com/mandarin/petching/domain/GenderType.java
+++ src/main/java/com/mandarin/petching/domain/GenderType.java
@@ -1,5 +1,15 @@
 package com.mandarin.petching.domain;
 
 public enum GenderType {
-    MALE, FEMALE
+    MALE("남자"), FEMALE("여자");
+
+    private final String description;
+
+    GenderType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
