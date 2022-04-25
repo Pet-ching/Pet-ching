0c0f6f1a3f68511bec5367368046bbc21e9c5644
taehyeok <ajaj3122@naver.com>
2022-04-25 12:21:27 +0900

    Fix: dto

--- src/main/java/com/mandarin/petching/dto/MemberFormDto.java
+++ src/main/java/com/mandarin/petching/dto/MemberFormDto.java
@@ -17,9 +17,9 @@ import java.time.LocalDate;
 @Getter @Setter
 public class MemberFormDto {
 
-    @NotBlank(message = "아이디는 필수 입력 값입니다.")
-    @Valid
-    private String userId;
+//    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+//    @Valid
+//    private String userId;
 
     @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
     @Length(min=8, max=16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
