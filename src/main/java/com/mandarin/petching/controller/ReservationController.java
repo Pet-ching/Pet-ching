3728566d9f4f8c0a8f423b4a9c9f842fd4e3d6e8
2cleanwater <2cleanwater@naver.com>
2022-04-26 20:52:29 +0900

    Fix : reservation null bug

--- src/main/java/com/mandarin/petching/controller/ReservationController.java
+++ src/main/java/com/mandarin/petching/controller/ReservationController.java
@@ -31,6 +31,7 @@ public class ReservationController {
         model.addAttribute("petOwner", member);
         model.addAttribute("petSitter", petSitter);
         model.addAttribute("petList", petList);
+        model.addAttribute("petListEmpty", petList.isEmpty());
 
         return "reservation";
     }
