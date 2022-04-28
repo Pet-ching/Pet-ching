6911de0a0f8745f5e3df2b2aea82aa34086c373a
seungyeonpark <siaayeon@gmail.com>
2022-04-28 09:05:12 +0900

    Fix: board list

--- src/main/java/com/mandarin/petching/repository/BoardRepository.java
+++ src/main/java/com/mandarin/petching/repository/BoardRepository.java
@@ -14,9 +14,8 @@ import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    List<Board> findByTitle(String title);
     List<Board> findByTitleOrContent(String title, String content);
-    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);//제목이나 내용으로 검색
+    Page<Board> findByBoardTypeOrderByIdDesc(BoardType boardType, Pageable pageable);
 
 
     //김귀영 추가
